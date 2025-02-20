3446eabafc0315abc7ea4586a2631d79e0944231
li-veronica <93553535+li-veronica@users.noreply.github.com>
2025-02-20 18:10:22 +0000

    calling add function from the main

--- merge_test/main.py
+++ merge_test/main.py
@@ -1,4 +1,10 @@
 from utils import add
 
 def main():
-    print("You are on the Master branch")
+    print("You are on the feature a branch")
+    a = 10
+    b = 50
+    result = add(a,b)
+    print(f"The result of the addition is {result}")
+
+main()
